4588e3266867e88926f69851a80fb0b2405dcdb3
한지호 <hanho1312@gmail.com>
2024-06-03 17:02:56 +0900

    불필요파일정리, blob데이터처리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import SignUp from './pages/login/SignUp';
+import GoodsList from './pages/board/GoodsList';
+import AddGoods from './pages/board/AddGoods';
+import EditGoods from './pages/board/EditGoods';
+import GoodDetail from './pages/board/GoodDetail';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="signup" element={<SignUp />} />
+                    <Route path="goods" element={<GoodsList />} />
+                    <Route path="add-goods" element={<AddGoods />} />
+                    <Route path="edit-goods/:id" element={<EditGoods />} />
+                    <Route path="goods/:id" element={<GoodDetail />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
