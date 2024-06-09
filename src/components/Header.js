fb048cdfa928849b2e4d84baf16d1d27e78f57f9
Han <hanho1312@gmail.com>
2024-06-09 21:33:47 +0900

    헤더 변경사항 커밋

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,27 @@
+// src/components/Header.js
 import React, { useContext, useEffect, useState } from 'react';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { fetchMenus, buildMenuTree } from '../api';
 import { AppBar, Toolbar, Typography, Button, Menu, MenuItem, Link, Box } from '@mui/material';
 import AuthContext from '../context/AuthContext';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+    },
+    typography: {
+        h6: {
+            fontWeight: 700,
+            fontSize: '1.25rem',
+        },
+    },
+});
 
 const Header = () => {
     const { user, logout } = useContext(AuthContext);
@@ -37,9 +56,7 @@ const Header = () => {
 
     const handleMenuClick = (menu) => {
         handleMenuClose();
-        console.log(menu.MENU_TYPE);
-        navigate(menu.MENU_URL);
-        if (menu.MENU_TYPE === 1) {
+        if (menu.MENU_TYPE === '1') {
             navigate(menu.MENU_URL);
         }
     };
@@ -48,8 +65,8 @@ const Header = () => {
         <MenuItem
             key={menu.MENU_NO}
             onClick={() => handleMenuClick(menu)}
-            component={menu.MENU_TYPE === 1 ? RouterLink : 'div'}
-            to={menu.MENU_TYPE === 1 ? menu.MENU_URL : ''}
+            component={menu.MENU_TYPE === '1' ? RouterLink : 'div'}
+            to={menu.MENU_TYPE === '1' ? menu.MENU_URL : ''}
         >
             {menu.MENU_NAME}
             {menu.children && menu.children.length > 0 && (
@@ -57,7 +74,7 @@ const Header = () => {
                     anchorEl={menuAnchor}
                     open={Boolean(menuAnchor && selectedMenu === menu)}
                     onClose={handleMenuClose}
-                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
                     transformOrigin={{ vertical: 'top', horizontal: 'left' }}
                 >
                     {menu.children.map(child => renderMenu(child))}
@@ -67,54 +84,59 @@ const Header = () => {
     );
 
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    <Link component={RouterLink} to="/" color="inherit" underline="none">
-                        Shopping
-                    </Link>
-                </Typography>
-                {menus.length > 0 && (
-                    <Box>
-                        {menus.map(menu => (
-                            <Button
-                                key={menu.MENU_NO}
-                                color="inherit"
-                                onClick={(e) => handleMenuOpen(e, menu)}
+        <ThemeProvider theme={theme}>
+            <AppBar position="static" color="primary">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
+                        <Link component={RouterLink} to="/" color="inherit" underline="none">
+                            Shopping
+                        </Link>
+                    </Typography>
+                    {menus.length > 0 && (
+                        <Box>
+                            {menus.map(menu => (
+                                <Button
+                                    key={menu.MENU_NO}
+                                    color="inherit"
+                                    onClick={(e) => handleMenuOpen(e, menu)}
+                                >
+                                    {menu.MENU_NAME}
+                                </Button>
+                            ))}
+                            <Menu
+                                anchorEl={menuAnchor}
+                                open={Boolean(menuAnchor)}
+                                onClose={handleMenuClose}
+                                anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                                transformOrigin={{ vertical: 'top', horizontal: 'left' }}
                             >
-                                {menu.MENU_NAME}
+                                {selectedMenu && selectedMenu.children.map(child => renderMenu(child))}
+                            </Menu>
+                        </Box>
+                    )}
+                    {user && (
+                        <>
+                            <Button color="inherit" component={RouterLink} to="/add-menu">
+                                Add Menu
                             </Button>
-                        ))}
-                        <Menu
-                            anchorEl={menuAnchor}
-                            open={Boolean(menuAnchor)}
-                            onClose={handleMenuClose}
-                            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-                            transformOrigin={{ vertical: 'top', horizontal: 'left' }}
-                        >
-                            {selectedMenu && selectedMenu.children.map(child => renderMenu(child))}
-                        </Menu>
-                    </Box>
-                )}
-                {user && (
-                    <>
-                        <Button color="inherit" onClick={logout}>
-                            Logout
-                        </Button>
-                    </>
-                )}
-                {!user && (
-                    <>
-                        <Button color="inherit" component={RouterLink} to="/login">
-                            Login
-                        </Button>
-                        <Button color="inherit" component={RouterLink} to="/signup">
-                            Sign Up
-                        </Button>
-                    </>
-                )}
-            </Toolbar>
-        </AppBar>
+                            <Button color="inherit" onClick={logout}>
+                                Logout
+                            </Button>
+                        </>
+                    )}
+                    {!user && (
+                        <>
+                            <Button color="inherit" component={RouterLink} to="/login">
+                                Login
+                            </Button>
+                            <Button color="inherit" component={RouterLink} to="/signup">
+                                Sign Up
+                            </Button>
+                        </>
+                    )}
+                </Toolbar>
+            </AppBar>
+        </ThemeProvider>
     );
 };
 
