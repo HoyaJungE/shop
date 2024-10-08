e24c23b0c8eea094b4db4295f3a6c0e9017361bb
한지호 <hanho1312@gmail.com>
2024-10-08 15:46:52 +0900

    모달팝업수정

--- src/components/CustomModal.js
+++ src/components/CustomModal.js
@@ -1,16 +1,28 @@
 // src/components/CustomModal.js
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyledDialog, StyledDialogTitle} from "./styles";
 import {DialogContent, IconButton} from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 
-const CustomModal = ({ open, onClose, title, children }) => {
+const CustomModal = ({ open = false, onClose, title, children }) => {
+
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const closeModal = () =>{
+        setModalVisible(false);
+        onClose();
+    }
+
+    useEffect(() =>{
+        setModalVisible(open);
+    }, [open])
+
     return (
-        <StyledDialog open={open} onClose={onClose}>
+        <StyledDialog open={modalVisible} onClose={closeModal}>
             <StyledDialogTitle>
                 {title}
-                <IconButton onClick={onClose}>
-                    <CloseIcon/>
+                <IconButton onClick={closeModal}>
+                    <CloseIcon />
                 </IconButton>
             </StyledDialogTitle>
             <DialogContent>
--- src/pages/role/ManageMemberRole.js
+++ src/pages/role/ManageMemberRole.js
@@ -1,194 +1,40 @@
 // src/pages/Role/ManageMemberRole.js
 import React, { useState, useEffect } from 'react';
-import {Link as RouterLink, useNavigate} from 'react-router-dom';
-import {fetchRoles, addRole, fetchMembers, addMemberRole, fetchRoleMembers} from '../../api';
-import {
-    Container,
-    TextField,
-    Button,
-    Box,
-    MenuItem,
-    Typography,
-    Grid,
-    Card,
-    CardMedia,
-    CardContent,
-    ListItem,
-    ListItemText,
-    IconButton, Modal
-} from '@mui/material';
+import {Container, TextField, Button} from '@mui/material';
 import CustomModal from "../../components/CustomModal";
 import {  } from '@mui/material/styles';
 
 const ManageMemberRole = () => {
-    const [RoleData, setRoleData] = useState({
-        UPPR_ROLE_NO: '',
-        ROLE_NO: '',
-        ROLE_NM:'',
-        ROLE_CN:''
-    });
-
-    const [memberData, setMemberData] = useState({
-        MEMBER_NO: '',
-        MEMBER_ID: '',
-        MEMBER_PASSWD: '',
-        MEMBER_NAME: '',
-        MEMBER_BIRTH: '',
-        MEMBER_EMAIL: '',
-        MEMBER_PHONE: '',
-        MEMBER_ZIPCODE: '',
-        MEMBER_ADDR1: '',
-        MEMBER_ADDR2: '',
-        MEMBER_DATE: '',
-        MEMBER_GRADE: '',
-        MEMBER_TOTAL: '',
-        MEMBER_LOG: '',
-        MEMBER_DELETE: '',
-        SMS_AGREE: '',
-        EMAIL_AGREE: ''
-    });
-
-    const [memberRole, setMemberRole] = useState({
-        ROLE_NO: '',
-        MEMBER_ID: ''
-    });
-
-    const [Roles, setRoles] = useState([]);
-    const [members, setMembers] = useState([]);
-    const [roleMembers, setRoleMembers] = useState([]);
-    const [modalContents, setModalContents] = useState({});
-    const [error, setError] = useState({});
-    const navigate = useNavigate();
-    const loadRoles = async () => {
-        const data = await fetchRoles();
-        setRoles(data);
-    };
-
-    const loadMembers = async () => {
-        const data = await fetchMembers();
-        setMembers(data);
-    };
-
-    const loadRoleMembers = async () => {
-        const data = await fetchRoleMembers();
-        setRoleMembers(data);
-
-        setModalContents(
-            <Grid container spacing={4}>
-                {roleMembers.map(rm => (
-                    <ListItem
-                        key={rm.ROLE_NO}
-                        button
-                        component={RouterLink}
-                    >
-                        <ListItemText
-                            primary={rm.ROLE_NO}
-                            secondary={rm.MEMBER_ID}
-                        />
-                    </ListItem>
-                ))}
-            </Grid>
-        );
-    };
-
-    useEffect(() => {
-        loadRoles();
-        loadMembers();
-    }, []);
-
-    const handleChange = (e) => {
-        setRoleData({ ...RoleData, [e.target.name]: e.target.value });
-    };
-
-    const handleMemberChange = (e) => {
-        setMemberData({ ...memberData, [e.target.name]: e.target.value });
-    };
-
-    const validate = () => {
-        let tempErrors = {};
-        tempErrors.ROLE_NM = RoleData.ROLE_NM ? '' : 'Role name is required.';
-        setError(tempErrors);
-        return Object.values(tempErrors).every(x => x === '');
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await addMemberRole(memberRole);
-            navigate('/memberRole');
-        } catch (error) {
-            setError('Failed to update menu');
-        }
-    };
-
     const [modalOpen, setModalOpen] = useState(false);
 
-    const handleModalOpen = () => {
+    const openModal = () => {
         setModalOpen(true);
     };
 
-    const handleModaClose = () => {
+    const closeModal = () => {
         setModalOpen(false);
     };
 
     return (
         <Container>
-            <Button variant="contained" color="primary" onClick={loadRoleMembers}>
-                road rmData
+            <Button variant="contained" color="primary" onClick={openModal}>
+                Load Member Data
             </Button>
-
-            <Typography variant="h4" gutterBottom>
-                ManageMemberRole
-            </Typography>
-
-            <Button variant="contained" color="primary" onClick={handleModalOpen}>
-                Open Modal
-            </Button>
-                <CustomModal open={modalOpen} onClose={handleModaClose} title="Modal Title" children={modalContents}>
+            <CustomModal
+                title='Manage Member Role'
+                open={modalOpen}
+                onClose={closeModal}
+            >
+                <TextField label="Member Role" variant="outlined" fullWidth />
+                <Button
+                    onClick={closeModal}
+                    variant="contained"
+                    color="primary"
+                    style={{ marginTop: 16 }}
+                >
+                    Save
+                </Button>
             </CustomModal>
-
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                <Typography variant="h4" gutterBottom>
-                    RoleList
-                </Typography>
-                <Grid container spacing={4}>
-                    {Roles.map(Role => (
-                        <>
-                            <ListItem
-                                key={Role.ROLE_NO}
-                                button
-                                component={RouterLink}
-                                onClick={()=>{ memberRole.ROLE_NO = Role.ROLE_NO; console.log(memberRole); }}
-                            >
-                                <ListItemText
-                                    primary={Role.ROLE_NO}
-                                    secondary={Role.ROLE_NM}
-                                />
-                            </ListItem>
-                        </>
-                    ))}
-                </Grid>
-
-                <Typography variant="h4" gutterBottom>
-                    MemberList
-                </Typography>
-                <Grid container spacing={2}>
-                    {members.map(member => (
-                        <ListItem
-                            key={member.MEMBER_NO}
-                            button
-                            component={RouterLink}
-                        >
-                            <ListItemText
-                                primary={member.MEMBER_NO}
-                            />
-                            <ListItemText
-                                primary={member.MEMBER_ID}
-                            />
-                        </ListItem>
-                    ))}
-                </Grid>
-            </Box>
         </Container>
     );
 };
